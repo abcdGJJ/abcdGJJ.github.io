6df0b36814a15d95ce6d1a03474ff08f54844527
abcdGJJ <1392552862@qq.com>
2018-08-26 00:46:45 +0800

    Site updated: 2018-08-26 00:46:44

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Mon Aug 20 2018 10:47:25 GMT+0800 (GMT+08:00)
+// build time:Sun Aug 26 2018 00:46:43 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");function e(){return $(".header-inner").height()+CONFIG.sidebar.offset}function i(){var t=$(".footer-inner");var e=t.outerHeight(true)-t.outerHeight();var i=t.outerHeight(true)+e;return i}function r(t){return $("#sidebar").css({"margin-top":t})}function n(){var n=e();var a=i();var f=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var o=$("#content").height();if(n+f<o){t.affix({offset:{top:n-CONFIG.sidebar.offset,bottom:a}})}r(n).css({"margin-left":"initial"})}function a(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");n()}function f(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){a()}})}n();f()});
 //rebuild by neat 
