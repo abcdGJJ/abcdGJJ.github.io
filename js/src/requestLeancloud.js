424d6b462da3b0f6294db0fa78771b25d354b885
abcdGJJ <1392552862@qq.com>
2020-05-10 18:06:42 +0800

    Site updated: 2020-05-10 18:06:40

--- js/src/requestLeancloud.js
+++
@@ -1,3 +0,0 @@
-// build time:Sun May 10 2020 17:34:27 GMT+0800 (GMT+08:00)
-if(!sessionStorage.getItem("requestLeancloud")){window.superagent.get("https://gjj123.avosapps.us").end();sessionStorage.setItem("requestLeancloud","1")}
-//rebuild by neat 
